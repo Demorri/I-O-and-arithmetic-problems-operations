635937d9ebda8a56b29221f45e176493bc66e29a
Demorri <98537439+Demorri@users.noreply.github.com>
2022-02-05 19:33:44 +0200

    Add files via upload

---
+++ Separation of apples - 2/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n, k, apls;
+        Scanner num = new Scanner(System.in);
+        System.out.print("Enter the amount of apples: ");
+        k = num.nextInt();
+        System.out.print("Enter the number of pupils: ");
+        n = num.nextInt();
+        apls = k % n;
+        System.out.println("The amount of apples in the basket is: " + apls);
+    }
+}
