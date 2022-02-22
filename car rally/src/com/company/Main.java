b1505903060cbc4e66470bccf29878841ef16c4e
Demorri <98537439+Demorri@users.noreply.github.com>
2022-02-22 13:48:53 +0200

    Update Main.java

--- car rally/src/com/company/Main.java
+++ car rally/src/com/company/Main.java
@@ -8,12 +8,11 @@ public class Main {
 	int d, m, n;
 
     System.out.print("Route length: ");
-    Scanner a = new Scanner(System.in);
-    m = a.nextInt();
+    Scanner in = new Scanner(System.in);
+    m = in.nextInt();
 
     System.out.print("Daily route: ");
-    Scanner b = new Scanner(System.in);
-    n = a.nextInt();
+    n = in.nextInt();
 
     d = (m - 1 + n) / n;
     System.out.print("You will need " + d + " days.");
