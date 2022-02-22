af93d3757455d35a849a97ec6f6af74c0be7b49c
Demorri <98537439+Demorri@users.noreply.github.com>
2022-02-22 13:47:06 +0200

    Update Main.java

--- Exchange of values/src/com/company/Main.java
+++ Exchange of values/src/com/company/Main.java
@@ -8,14 +8,13 @@ public class Main {
 	    int a, b;
 
         System.out.print("Enter the A number: ");
-        Scanner num1 = new Scanner(System.in);
-        a = num1.nextInt();
+        Scanner in = new Scanner(System.in);
+        a = in.nextInt();
 
         b = a;
 
-        System.out.print("Enter B number: ");
-        Scanner num2 = new Scanner(System.in);
-        a = num2.nextInt();
+        System.out.print("Enter B number: ");    
+        a = in.nextInt();
 
         System.out.print(a + " " + b);
 
